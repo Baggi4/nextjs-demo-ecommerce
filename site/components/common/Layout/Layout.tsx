d51a40c84fa4621b14fd1ed40171caf2836ae365
StevenMad <stevenmadec@live.fr>
2022-12-14 00:34:44 +0100

    1.10

--- site/components/common/Layout/Layout.tsx
+++ site/components/common/Layout/Layout.tsx
@@ -5,7 +5,7 @@ import { useRouter } from 'next/router'
 import { CommerceProvider } from '@framework'
 import LoginView from '@components/auth/LoginView'
 import { useUI } from '@components/ui/context'
-import { Navbar, Footer } from '@components/common'
+import { Navbar, Footer, Banner } from '@components/common'
 import ShippingView from '@components/checkout/ShippingView'
 import CartSidebarView from '@components/cart/CartSidebarView'
 import { useAcceptCookies } from '@lib/hooks/useAcceptCookies'
@@ -117,6 +117,7 @@ const Layout: React.FC<Props> = ({
   return (
     <CommerceProvider locale={locale}>
       <div className={cn(s.root)}>
+        <Banner />
         <Navbar links={navBarlinks} />
         <main className="fit">{children}</main>
         <Footer pages={pageProps.pages} />
