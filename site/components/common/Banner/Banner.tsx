59d8dad8eee402f5560701cfe4dd8ef737ff3bd7
StevenMad <stevenmadec@live.fr>
2022-12-15 19:15:24 +0100

    banner-animation

--- site/components/common/Banner/Banner.tsx
+++ site/components/common/Banner/Banner.tsx
@@ -3,22 +3,24 @@ import s from './Banner.module.css'
 const Banner = () => {
   return (
     <div className={s.root}>
-      <div className="bg-indigo-600">
-        <div className="mx-auto max-w-7xl py-3 px-3 sm:px-6 lg:px-8">
-          <div className="flex justify-evenly items-center">
-            <div className={s.title}>
-              <span className={s.sdtitle}>
-                We announced a new product!
-                <a href="./" className="underline ml-1">
-                  Learn more
-                </a>
+      <div className="relative bg-indigo-600">
+        <div className="mx-auto max-w-md py-3 px-3 sm:px-6 lg:px-8">
+          <div className="flex flex-wrap items-center justify-between">
+            <div className="flex w-0 flex-1 items-center">
+              <span className={s.row}>
+                <span
+                  title="We announced a brand new product!"
+                  data-alt="Big news!"
+                ></span>
               </span>
-              <span className={s.sdtitle}>
-                Big news! We're excited to announce a brand new product.
-                <a href="./" className="underline ml-1">
-                  Learn more
-                </a>
-              </span>
+            </div>
+            <div className="inline-block">
+              <a
+                href="/"
+                className="rounded-md border underline border-transparent bg-white px-4 py-2 text-sm font-medium text-indigo-600 shadow-sm hover:bg-indigo-50"
+              >
+                Learn more
+              </a>
             </div>
           </div>
         </div>
